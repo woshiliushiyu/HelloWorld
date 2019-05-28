28ac59cecabb35664e4dc1714f7905849d0c23ca
woshiliushiyu <1010530278@qq.com>
2019-05-28 10:34:54 +0800

    添加 logger

--- liushiyu/src/main/java/com/liushiyu/controller/LSYMainController.java
+++ liushiyu/src/main/java/com/liushiyu/controller/LSYMainController.java
@@ -4,13 +4,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 public class LSYMainController {
+
+    static Logger logger;
 
     @RequestMapping(value = "/index")
     @ResponseBody
     public String index() {
         System.out.println("请求成功");
+        logger.info("开始请求成功"+"index");
         return "请求成功01";
     }
 }
