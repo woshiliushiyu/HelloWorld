912fb5d44f0f2318617249a522ae4a6e5797663d
woshiliushiyu <1010530278@qq.com>
2019-05-28 10:41:21 +0800

    logger 添加打印

--- liushiyu/src/main/java/com/liushiyu/controller/LSYMainController.java
+++ liushiyu/src/main/java/com/liushiyu/controller/LSYMainController.java
@@ -1,15 +1,16 @@
 package com.liushiyu.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.logging.Logger;
 
 @RestController
 public class LSYMainController {
 
-    static Logger logger;
+    private static final Logger logger = LoggerFactory.getLogger(LSYMainController.class);
 
     @RequestMapping(value = "/index")
     @ResponseBody
