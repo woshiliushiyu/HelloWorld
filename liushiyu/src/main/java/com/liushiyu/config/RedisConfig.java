9fafffb45aab83af9b238baea3a13fbd35e37baa
woshiliushiyu <1010530278@qq.com>
2019-06-03 15:17:00 +0800

    提交修改

--- liushiyu/src/main/java/com/liushiyu/config/RedisConfig.java
+++ liushiyu/src/main/java/com/liushiyu/config/RedisConfig.java
@@ -17,53 +17,53 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 import java.lang.reflect.Method;
 
-@Configuration
-@EnableCaching
-public class RedisConfig extends CachingConfigurerSupport {
+//@Configuration
+//@EnableCaching
+public class RedisConfig {//extends CachingConfigurerSupport {
 
-    /**
-     * 生成key的策略
-     * @return
-     */
-    @Bean
-    public KeyGenerator keyGenerator() {
-        return new KeyGenerator() {
-            @Override
-            public Object generate(Object target, Method method, Object... params) {
-                StringBuilder sb = new StringBuilder();
-                sb.append(target.getClass().getName());
-                sb.append(method.getName());
-                for (Object obj : params) {
-                    sb.append(obj.toString());
-                }
-                return sb.toString();
-            }
-        };
-    }
-
-    /**
-     * 管理缓存
-     */
+//    /**
+//     * 生成key的策略
+//     * @return
+//     */
 //    @Bean
-//    public CacheManager cacheManager(RedisTemplate redisTemplate) {
-//        RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
-//        return rcm;
+//    public KeyGenerator keyGenerator() {
+//        return new KeyGenerator() {
+//            @Override
+//            public Object generate(Object target, Method method, Object... params) {
+//                StringBuilder sb = new StringBuilder();
+//                sb.append(target.getClass().getName());
+//                sb.append(method.getName());
+//                for (Object obj : params) {
+//                    sb.append(obj.toString());
+//                }
+//                return sb.toString();
+//            }
+//        };
 //    }
-
-    /**
-     * RedisTemplate配置
-     */
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
-        StringRedisTemplate template = new StringRedisTemplate(factory);
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-        ObjectMapper om = new ObjectMapper();
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(om);
-        template.setValueSerializer(jackson2JsonRedisSerializer);
-        template.afterPropertiesSet();
-        return template;
-    }
+//
+//    /**
+//     * 管理缓存
+//     */
+////    @Bean
+////    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+////        RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
+////        return rcm;
+////    }
+//
+//    /**
+//     * RedisTemplate配置
+//     */
+//    @Bean
+//    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+//        StringRedisTemplate template = new StringRedisTemplate(factory);
+//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+//        ObjectMapper om = new ObjectMapper();
+//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+//        jackson2JsonRedisSerializer.setObjectMapper(om);
+//        template.setValueSerializer(jackson2JsonRedisSerializer);
+//        template.afterPropertiesSet();
+//        return template;
+//    }
 
 }
